412cf77d7716642d00cebbb264143428ee58c383
Vikhyath-vec <vikhyath.kothamasu@gmail.com>
2023-04-25 07:18:46 +0530

    identifying name from image

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,7 @@ from jinja2 import Template
 import pytesseract
 from PIL import Image
 from pyzbar import pyzbar
+import nltk
 
 # Timezone
 IST = pytz.timezone('Asia/Kolkata')
@@ -32,6 +33,18 @@ load_dotenv()
 
 # initialize pytesseract path
 pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_PATH")
+
+# download the required corpus and model data
+try:
+    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('taggers/averaged_perceptron_tagger')
+    nltk.data.find('chunkers/maxent_ne_chunker')
+    nltk.data.find('corpora/words')
+except LookupError:
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('maxent_ne_chunker')
+    nltk.download('words')
 
 # Database credentials
 DATABASE = os.getenv("DATABASE")
@@ -197,7 +210,37 @@ def get_details_from_image(img):
     barcodes = pyzbar.decode(img)
     for barcode in barcodes:
         barcodeData = barcode.data.decode("utf-8")
-    return text, barcodeData
+    
+    lines = text.splitlines()
+    sentences = []
+    for line in lines:
+        sentences += nltk.sent_tokenize(line)
+
+    name_detected = ""
+    flag = 0
+
+    for sentence in sentences:
+        words = nltk.word_tokenize(sentence)
+        tagged = nltk.pos_tag(words)
+        # use the named entity recognizer to extract named entities
+        named_entities = nltk.ne_chunk(tagged)
+        # loop over each named entity
+        for entity in named_entities:
+            # if the entity is a person and has a label of 'PERSON'
+            if hasattr(entity, 'label') and entity.label() == 'PERSON' and flag == 0:
+                # check if the person is an Indian name by looking for Indian prefixes
+                name_detected = ' '.join(c[0] for c in entity.leaves() if c[0] not in ['Mr.', 'Ms.', 'Mrs.', 'Dr.'])
+                flag = 1
+                break
+        if flag == 1:
+            break
+
+    for line in lines:
+        if name_detected in line:
+            full_name = line
+            break
+    
+    return {"package_number": barcodeData, "owner_name": full_name}
 
 @app.get("/")
 async def root():
@@ -232,3 +275,6 @@ async def get_package(package_id: int, db: Session = Depends(get_db)):
     return {"message": "Package not found"}
 
 # send_email('Vikhyath', 'AWB1002', 'Goutham', ['cs20btech11056@iith.ac.in', 'cs20btech11042@iith.ac.in', 'cs19btech11051@iith.ac.in', 'es19btech11017@iith.ac.in'])
+# img = Image.open('images/test_image.jpeg').convert('L')
+# package = get_details_from_image(img)
+# print(package)
