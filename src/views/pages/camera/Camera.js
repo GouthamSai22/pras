7aaebc68c717cf26df698182e5bf0bba5296c860
boredtea <es19btech11017@iith.ac.in>
2023-04-25 20:52:01 +0530

    Add camera component

---
+++ src/views/pages/camera/Camera.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import Webcam from 'react-webcam'
+const WebcamComponent = () => <Webcam />
+const videoConstraints = {
+  width: 400,
+  height: 400,
+  facingMode: 'user',
+}
+const Camera = () => {
+  const [picture, setPicture] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('capturedImage', JSON.stringify(picture));
+  }, [picture]);
+
+  const webcamRef = React.useRef(null)
+  const capture = React.useCallback(() => {
+    const pictureSrc = webcamRef.current.getScreenshot()
+    setPicture(pictureSrc)
+    console.log("saving...")
+    console.log(typeof(picture))
+    
+  })
+  return (
+    <div>
+      <h2 className="mb-5 text-center">
+        React Photo Capture using Webcam Examle
+      </h2>
+      <div>
+        {picture == '' ? (
+          <Webcam
+            audio={false}
+            height={400}
+            ref={webcamRef}
+            width={400}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        ) : (
+          <img src={picture} />
+        )}
+      </div>
+      <div>
+        {picture != '' ? (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              setPicture()
+            }}
+            className="btn btn-primary"
+          >
+            Retake
+          </button>
+        ) : (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              capture()
+            }}
+            className="btn btn-danger"
+          >
+            Capture
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+export default Camera
