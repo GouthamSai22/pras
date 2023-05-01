9a48014b17d46c4b1d887add10e8b56028e833e9
boredtea <es19btech11017@iith.ac.in>
2023-05-01 23:39:33 +0530

    Add buttons and modal

--- src/views/pages/viewPackage/ViewCollected.js
+++ src/views/pages/viewPackage/ViewCollected.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./viewCollected.css"; // import your custom styles
+import CIcon from "@coreui/icons-react";
+import { cilBarcode, cilPencil, cilTrash } from "@coreui/icons";
+import {
+  CButton,
+  CTooltip,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
+  CAlert,
+} from "@coreui/react";
 
-function viewCollected({ }) {
+function viewCollected({}) {
+  const [deleteVisible, setDeleteVisible] = useState(false);
+  const [delAlertVisible, setDelAlertVisible] = useState(false);
+
   const packages = [
-    { id: 1, pickupDate: '2022-05-01', parcelType: 'Box', postDetails: 'Sent by John' },
-    { id: 2, pickupDate: '2022-05-02', parcelType: 'Envelope', postDetails: 'Sent by Mary' },
-    { id: 3, pickupDate: '2022-05-03', parcelType: 'Package', postDetails: 'Sent by Bob' },
+    {
+      id: 1,
+      pickupDate: "2022-05-01",
+      parcelType: "Box",
+      postDetails: "Sent by John",
+    },
+    {
+      id: 2,
+      pickupDate: "2022-05-02",
+      parcelType: "Envelope",
+      postDetails: "Sent by Mary",
+    },
+    {
+      id: 3,
+      pickupDate: "2022-05-03",
+      parcelType: "Package",
+      postDetails: "Sent by Bob",
+    },
   ];
 
   return (
@@ -18,6 +48,9 @@ function viewCollected({ }) {
             <th>Date of Pickup</th>
             <th>Type of Parcel</th>
             <th>Post Details</th>
+            <th></th>
+            <th></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -27,6 +60,65 @@ function viewCollected({ }) {
               <td>{pkg.pickupDate}</td>
               <td>{pkg.parcelType}</td>
               <td>{pkg.postDetails}</td>
+              <td>
+                <CTooltip content="Collect" placement="bottom">
+                  <CButton color="light">
+                    <CIcon icon={cilBarcode}></CIcon>
+                  </CButton>
+                </CTooltip>
+              </td>
+              <td>
+                <CTooltip content="Edit" placement="bottom">
+                  <CButton color="light">
+                    <CIcon icon={cilPencil}></CIcon>
+                  </CButton>
+                </CTooltip>
+              </td>
+              <td>
+                <CTooltip content="Delete" placement="bottom">
+                  <CButton
+                    color="light"
+                    onClick={() => setDeleteVisible(!deleteVisible)}
+                  >
+                    <CIcon icon={cilTrash}></CIcon>
+                    <CModal
+                      alignment="center"
+                      scrollable
+                      visible={deleteVisible}
+                      onClose={() => setDeleteVisible(false)}
+                    >
+                      <CModalHeader>
+                        <CModalTitle>Delete Entry</CModalTitle>
+                      </CModalHeader>
+                      <CModalBody>
+                        <p>Are you sure you want to delete this entry?</p>
+                      </CModalBody>
+                      <CModalFooter>
+                        <CButton
+                          color="secondary"
+                          onClick={() => setDeleteVisible(false)}
+                        >
+                          Cancel
+                        </CButton>
+
+                        <CButton color="primary" onClick={() => setDeleteVisible(false)}>Delete</CButton>
+
+
+                        {/* <CAlert
+                          color="primary"
+                          dismissible
+                          visible={delAlertVisible}
+                          onClose={() => setDelAlertVisible(false)}
+                        >
+                          Package Deleted!
+                        </CAlert>
+                        <CButton color="primary" onClick={() => setDelAlertVisible(true)}>Delete</CButton> */}
+                      
+                      </CModalFooter>
+                    </CModal>
+                  </CButton>
+                </CTooltip>
+              </td>
             </tr>
           ))}
         </tbody>
